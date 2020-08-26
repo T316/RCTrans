5d504d4460c0d8c19dfacc5bce0f017f06e9c78a
Todor Yordanov <todor.yordanov@mail.bg>
2020-08-26 13:24:00 +0300

    Project init

---
+++ RCTrans/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RCTrans.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
