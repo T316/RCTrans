46b30414df56e6c0f644963d49503da9e2cab2ec
Todor Yordanov <todor.yordanov@mail.bg>
2020-08-26 14:07:10 +0300

    Add Autopark and Contacts. Modify navbar.

--- RCTrans/Controllers/HomeController.cs
+++ RCTrans/Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace RCTrans.Controllers
             return View();
         }
 
+        public IActionResult Autopark()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
